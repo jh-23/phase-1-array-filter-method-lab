34b43dcb46773a467fb708fe60433ee54003b098
John Heger <heger7@gmail.com>
2023-10-17 15:58:36 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
+
+
